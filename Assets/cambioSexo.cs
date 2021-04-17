b4c92f540653a16ff6e5eb907da0e4e2fc5274ee
Alberth-h <jesalbertohyfx@hotmail.com>
2021-04-16 20:09:00 -0700

    cambio de SEXOOOOOOOOOOOOOO

---
+++ Assets/cambioSexo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cambioSexo : MonoBehaviour
+{
+    public GameObject male;
+    public GameObject female;
+
+    public void changeMale()
+    {
+        male.SetActive(true);
+        female.SetActive(false);
+    }
+
+    public void changeFemale()
+    {
+        male.SetActive(false);
+        female.SetActive(true);
+    }
+}
