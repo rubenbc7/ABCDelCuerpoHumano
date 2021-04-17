dc3f4106fbe5dd891f3251ebab054ea193ebef76
Alberth-h <jesalbertohyfx@hotmail.com>
2021-04-16 21:18:59 -0700

    rotacion

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float rotationSpeed = 100f;
+    bool dragging = false;
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent <Rigidbody>();
+    }
+
+    // Update is called once per frame
+   /* void OnMouseDrag(){
+        dragging = true;
+        Debug.Log("Detecta mouse ");
+    }*/
+    void Update()
+    {
+        if(Input.GetMouseButtonUp(0)){
+            dragging = false;
+        }
+        else if(Input.GetMouseButtonDown(0)){
+            dragging = true;
+            Debug.Log("Detecta mouse ");
+        }
+    }
+    void FixedUpdate(){
+        if(dragging){
+            float x = Input.GetAxis("Mouse X") * rotationSpeed * Time.fixedDeltaTime;
+            //float y = Input.GetAxis("Mouse Y") * rotationSpeed * Time.fixedDeltaTime;
+
+            rb.AddTorque(Vector3.down*x);
+            //rb.AddTorque(Vector3.right*y);
+        }
+    }
+}
---
+++ Assets/Scripts/cambioSexo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cambioSexo : MonoBehaviour
+{
+    public GameObject male;
+    public GameObject female;
+
+    public void changeMale()
+    {
+        male.SetActive(true);
+        female.SetActive(false);
+    }
+
+    public void changeFemale()
+    {
+        //femalePosition.Rotate(0.0f, 0.0f, 0.0f);
+        male.SetActive(false);
+        female.SetActive(true);
+    }
+}
