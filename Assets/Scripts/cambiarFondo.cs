928d1a8de0b80e265ba5a471f674f06b43c7e118
Alberth-h <jesalbertohyfx@hotmail.com>
2021-05-08 00:59:23 -0700

    fondos

---
+++ Assets/Scripts/cambiarFondo.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cambiarFondo : MonoBehaviour
+{
+    public GameObject espacio;
+    public GameObject espacio1;
+    public GameObject rocasAgua;
+    public GameObject rocasAgua1;
+    public GameObject sailoor;
+    public GameObject sailoor1;
+    public GameObject vaper;
+    public GameObject vaper1;
+
+    int valor = 0;
+
+    public void changeFondo()
+    {
+        valor++;
+        if(valor > 3)
+        {
+            valor = 0;
+        }
+        if(valor == 0)
+        {
+            espacio.SetActive(true);
+            espacio1.SetActive(true);
+            rocasAgua.SetActive(false);
+            rocasAgua1.SetActive(false);
+            sailoor.SetActive(false);
+            sailoor1.SetActive(false);
+            vaper.SetActive(false);
+            vaper1.SetActive(false);
+        }
+        if(valor == 1)
+        {
+            espacio.SetActive(false);
+            espacio1.SetActive(false);
+            rocasAgua.SetActive(true);
+            rocasAgua1.SetActive(true);
+            sailoor.SetActive(false);
+            sailoor1.SetActive(false);
+            vaper.SetActive(false);
+            vaper1.SetActive(false);
+        }
+        if(valor == 2)
+        {
+            espacio.SetActive(false);
+            espacio1.SetActive(false);
+            rocasAgua.SetActive(false);
+            rocasAgua1.SetActive(false);
+            sailoor.SetActive(true);
+            sailoor1.SetActive(true);
+            vaper.SetActive(false);
+            vaper1.SetActive(false);
+        }
+        if(valor == 3)
+        {
+            espacio.SetActive(false);
+            espacio1.SetActive(false);
+            rocasAgua.SetActive(false);
+            rocasAgua1.SetActive(false);
+            sailoor.SetActive(false);
+            sailoor1.SetActive(false);
+            vaper.SetActive(true);
+            vaper1.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/gifAnimator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gifAnimator : MonoBehaviour
+{
+    public Texture2D[] frames;
+    private float fps = 60.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float index = Time.time * fps;
+        index = index % frames.Length;
+        GetComponent<Renderer>().material.mainTexture = frames[(int)index];
+    }
+}
