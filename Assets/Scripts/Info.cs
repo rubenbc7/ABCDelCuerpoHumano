21eb6a46336dfa3c622e012253e85b5fc63fc0aa
rubenbc7 <rubenborbollacastelo@gmail>
2021-05-07 23:41:09 -0700

    COCO

---
+++ Assets/Scripts/Info.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Info : MonoBehaviour
+{
+    public GameObject Huesos;
+    public GameObject Male;
+    public GameObject Female;
+    public GameObject Izquierda;
+    public GameObject Derecha;
+    public GameObject i;
+    public GameObject COCO;
+    public GameObject txt;
+    public GameObject X;
+
+    public void menuON(){
+        Huesos.SetActive(false);
+        Male.SetActive(false);
+        Female.SetActive(false);
+        Izquierda.SetActive(false);
+        Derecha.SetActive(false);
+        i.SetActive(false);
+        COCO.SetActive(true);
+        txt.SetActive(false);
+        X.SetActive(true);
+    }
+
+        public void menuOFF(){
+        Huesos.SetActive(true);
+        Male.SetActive(true);
+        Female.SetActive(true);
+        Izquierda.SetActive(true);
+        Derecha.SetActive(true);
+        i.SetActive(true);
+        COCO.SetActive(false);
+        txt.SetActive(true);
+        X.SetActive(false);
+    }
+
+}
